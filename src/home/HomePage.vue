69c6e380429c31e714a057e0caad0dfaa8cac203
Jonny Hork <JonnyHork@gmail.com>
2018-07-18 18:51:03 -0600

    set up file structure, render homePage and add build RobotBuilder component.

---
+++ src/home/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" alt="">
+    </div>
+    <div class="get-started">
+      <a @click.prevent='sayHi()' href="">Get started</a> Build Your First Robot
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+  methods: {
+    sayHi() {
+      console.log('Hey')
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+</style>
